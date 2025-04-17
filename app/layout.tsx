560ad710a5bd2d0b973f3b75aabc3a555511843d
Kamran <155973381+Kamrancodex@users.noreply.github.com>
2025-04-17 20:07:01 +0530

    nav tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "FA-RAYS FAMILY",
+  description:
+    "FA-RAYS FAMILY Restaurant 70+ YEARS 3 GENERATIONS 1 GREAT RESTAURANT",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
